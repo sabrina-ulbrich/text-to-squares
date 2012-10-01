e7fe7afcba705c734ac5a18cf128ab4b83b93c57
sabrina-ulbrich <su@evolos.de>
2012-10-01 21:40:26 +0200

    railsapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TextToSquares::Application.config.secret_token = '09eeb2bf89794962aa3373993a26996bdc6ae31dd7816246d48beb03de2cc887112a26777169d88315098bee5b329b227b28b9b3f6e23345e2a5c1fe49747f30'
